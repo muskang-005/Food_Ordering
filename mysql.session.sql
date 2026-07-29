0cb0903f3a8b3db62503299f04e697b1f34f77ca
Muskan Singh <muskansinghrkl@gmail.com>
2026-07-30 01:16:06 +0530

    Create mysql.session.sql

---
+++ mysql.session.sql
@@ -0,0 +1,8 @@
+INSERT INTO wallet_history (id, user_id, amount, type, created_at)
+VALUES (
+    id:int,
+    user_id:int,
+    'amount:decimal',
+    'type:varchar',
+    'created_at:timestamp'
+  );
